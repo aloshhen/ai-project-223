e4ac9a7b0fd446d78c1212b4c3b19b811fbc4738
aloshhen <166549924+aloshhen@users.noreply.github.com>
2026-02-03 21:57:17 +0100

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        csgo: {
+          orange: '#f59e0b',
+          yellow: '#eab308',
+          dark: '#0f0f0f',
+          darker: '#050505',
+          gray: '#1a1a1a'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
